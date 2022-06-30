46c1e5e0329c0be2088717def2ade0bd8c95cce2
shoma564 <shomaigu@gmail.com>
2022-06-30 02:07:57 +0000

    説明追加

--- kadai/hukasa/hukasa.c
+++ kadai/hukasa/hukasa.c
@@ -9,9 +9,9 @@ int a[N+1][N+1]={{0,0,0,0,0,0,0},
                  {0,1,0,0,1,0,1},
                  {0,0,0,0,1,1,0}};
 int v[N+1];
-void dfs(int);
+void dfs(int);;/*訪問したかの記しておく*/
 
-int main(void)
+int main(void)/*実際に探索する*/
 {
   int i;
   for (i=1; i<=N; i++)
@@ -25,7 +25,7 @@ void dfs(int i)
 {
  int j;
  v[i]=1;
- for (j=1; j<=N; j++){
+ for (j=1; j<=N; j++){/*印を初期化する*/
    if (a[i][j]==1&&v[j]==0){
      printf("%d->%d",i,j);
      dfs(j);
